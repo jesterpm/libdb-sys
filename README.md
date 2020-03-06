77898f10bdef0cfd5de646a0d21e8d15b372c562
Karl Fischer <fishi0x01@users.noreply.github.com>
2020-03-07 00:59:30 +0100

    db 5.3.21 and features (#1)

---
+++ README.md
@@ -0,0 +1,17 @@
+**Status**
+
+![CI](https://github.com/fishi0x01/libdb-sys/workflows/CI/badge.svg)
+
+# libdb-sys
+
+Bindings for Berkeley DB 4.8.x and 5.3.x.
+
+This is a humble fork from [jesterpm's](https://github.com/jesterpm/libdb-sys) libdb-sys.
+
+## Features
+
+`v4_8` uses bindings for Berkeley DB 4.8.x.
+
+`v5_3` uses bindings for Berkeley DB 5.3.x.
+
+By default, Berkeley DB 5.3.x is used. 
---
+++ build_4_8.rs
@@ -0,0 +1,43 @@
+extern crate bindgen;
+
+use std::path::{PathBuf, Path};
+use std::process::Command;
+
+pub const SOURCE_DIR: &'static str = "db-4.8.30";
+
+pub fn build_unix(out_dir: &str) {
+    let build_dir = Path::new(SOURCE_DIR).join("build_unix");
+    Command::new("../dist/configure")
+        .arg(&format!("--prefix={}", out_dir))
+        .arg("--with-gnu-ld")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+    Command::new("make")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+    Command::new("make")
+        .arg(&"install")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", Path::new(&out_dir).join("lib").to_string_lossy());
+    println!("cargo:rustc-link-lib=static=db");
+}
+
+pub fn generate_bindings(out_dir: &str) {
+    let bindings = bindgen::Builder::default()
+        .header("db-4.8.30/build_unix/db.h")
+        .derive_copy(true)
+        .derive_debug(true)
+        .derive_default(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(&out_dir);
+    bindings
+        .write_to_file(out_path.join("ffi.rs"))
+        .expect("Couldn't write bindings!");
+}
